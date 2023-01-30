e6025b9cedeb9dbd44129fd97e40e1a43eda36d1
Etienne Salupo-Corvo <etienne.salupocorvo@gmail.com>
2023-01-30 23:36:09 +0100

    feat adding run movement

--- Player/PlayerController.gd
+++ Player/PlayerController.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
 @export var speed_walk: float = 1.0;
+@export var speed_run_factor: float = 2.0;
 @export var camera: Camera3D;
 @onready var animation_tree = $AnimationTree;
 @onready var cloth_animations = $ClothAnimations;
@@ -13,7 +14,6 @@ var _is_inventory_visible = false;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animation_tree.active = true;
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -30,12 +30,17 @@ func _input(event):
 func _handle_movement():
 	var direction_x = Input.get_action_strength("right") - Input.get_action_strength("left");
 	var direction_z = Input.get_action_strength("down") - Input.get_action_strength("up");
-	var speed = speed_walk / _speed_walk_factor;
-	velocity = Vector3(direction_x*speed, 0, direction_z*speed);
+	var speed_run = max(1, Input.get_action_strength("run") * speed_run_factor);
+	var speed = (speed_walk / _speed_walk_factor) * speed_run;
+	velocity = Vector3(direction_x*speed, 0, direction_z*speed).normalized() * speed;
 
 func _handle_animation():
 	cloth_animations.rotation.x = camera.rotation.x;
 	if velocity.x != 0:
 		animated_sprite_3d.scale.x = 1 if velocity.x > 0 else -1;
-	animation_tree.set("parameters/conditions/isWalking", velocity != Vector3.ZERO);
-	animation_tree.set("parameters/conditions/isIdle", velocity == Vector3.ZERO);
+	var is_idle = velocity == Vector3.ZERO;
+	var is_walking = velocity != Vector3.ZERO and velocity.length() < 1;
+	var is_running = velocity != Vector3.ZERO and velocity.length() > 1;
+	animation_tree.set("parameters/conditions/isIdle", is_idle);
+	animation_tree.set("parameters/conditions/isWalking", is_walking);
+	animation_tree.set("parameters/conditions/isRunning", is_running);
