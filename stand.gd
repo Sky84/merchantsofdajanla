7268f190d96c779037491d8269fc1c8c647200fe
Majstruk Guillaume <guillaume.majstruk@hotmail.fr>
2023-02-02 01:15:35 +0100

    add: stand model

---
+++ stand.gd
@@ -0,0 +1,11 @@
+extends StaticBody3D;
+
+@onready var _area := $Area3D;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("toto");
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
