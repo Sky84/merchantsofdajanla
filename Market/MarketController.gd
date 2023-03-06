b3a128c5634a8fbb00f6496ff330ecd9b3173794
Etienne Salupo-Corvo <etienne.salupocorvo@gmail.com>
2023-03-06 11:47:10 +0100

    feat setting current_price of items in stand

--- Market/MarketController.gd
+++ Market/MarketController.gd
@@ -1,13 +1,16 @@
 extends Node
 
 func get_items(container_id: String) -> Dictionary:
-	var slots = ContainersController.get_container_data(container_id).duplicate(true);
+	var slots = ContainersController.get_container_data(container_id);
 	for x in slots:
 		for y in slots[x]:
 			var item = slots[x][y];
 			if item:
-				item.current_price = get_current_price(item.id);
+				item.current_price = get_current_price(item);
 	return slots;
 
-func get_current_price(item_id: String):
-	return GameItems.get_item(item_id).base_price;
+func get_current_price(item: Dictionary):
+	var current_price = item.base_price;
+	if 'current_price' in item:
+		current_price = item.current_price;
+	return current_price;
--- Stand/TransactionView/StandView.gd
+++ Stand/TransactionView/StandView.gd
@@ -2,6 +2,7 @@ extends InventoryView
 class_name StandTransactionView
 
 @onready var item_value_label = $VBoxContainer/TextureRect/Value;
+@export var MAX_PRICE: int;
 
 func _ready():
 	pass
@@ -11,20 +12,26 @@ func open(_container_id: String) -> void:
 	_load_container_config();
 	var items = MarketController.get_items(container_id);
 	_update_items(items);
-	item_value_label.text = str(get_item().current_price);
 	visible = true;
 
 func get_item() -> Dictionary:
-	var item = MarketController.get_items(container_id)[0];
-	if  not "current_price" in item:
-		item.current_price = 999;
+	var item = MarketController.get_items(container_id)[0][0];
 	return item;
+
+func _update_items(slots: Dictionary) -> void:
+	super._update_items(slots);
+	_update_price();
 
 # We know that going to be called just once because there is only one item for stand
 func _update_custom_item(slot_x: int, slot_y: int, slot_instance: SlotButton) -> void:
 	if slot_instance.get_child_count() > 0:
 		var item_instance: StandItemButton = slot_instance.get_child(0);
 		item_instance._set_price(get_item().current_price);
+
+func _update_price() -> void:
+	var item = get_item();
+	var current_price = MAX_PRICE if not "current_price" in item else item.current_price;
+	item_value_label.text = str(current_price);
 
 func _input(event):
 	pass
@@ -34,3 +41,15 @@ func _handle_mouse_click(event: InputEventMouseButton) -> void:
 
 func close() -> void:
 	visible = false;
+
+func _on_minus_button_pressed():
+	var item = get_item();
+	item.current_price = clampi(item.current_price - 1, 0, MAX_PRICE);
+	var items = MarketController.get_items(container_id);
+	_update_items(items);
+
+func _on_plus_button_pressed():
+	var item = get_item();
+	item.current_price = clampi(item.current_price + 1, 0, MAX_PRICE);
+	var items = MarketController.get_items(container_id);
+	_update_items(items);
