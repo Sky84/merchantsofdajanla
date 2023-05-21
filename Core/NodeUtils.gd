5aff279ab8a09194a2215ee39b5472c85897aac3
Etienne SC <etienne.salupocorvo@gmail.com>
2023-05-21 22:42:08 +0200

    feat : Wait Action done

--- Core/NodeUtils.gd
+++ Core/NodeUtils.gd
@@ -12,36 +12,16 @@ func get_map_item_id(map_item: MapItem) -> String:
 	var map_id = PackedStringArray([map_item.name, map_item.position.x, map_item.position.y, map_item.position.z])
 	return '_'.join(map_id);
 
-func get_random_reachable_point(nav_mesh: NavigationMesh, navigation_agent: NavigationAgent3D, gridmap_controller: GridMapController):
-	if nav_mesh and nav_mesh.get_polygon_count() > 0:
-		# Récupérer les sommets du maillage de navigation
-		var vertices = nav_mesh.get_vertices();
-		
-		# Trouver les limites du maillage en parcourant tous les sommets
-		var minX = INF;
-		var maxX = -INF;
-		var minZ = INF;
-		var maxZ = -INF;
-		
-		for vertex in vertices:
-			minX = min(minX, vertex.x);
-			maxX = max(maxX, vertex.x);
-			minZ = min(minZ, vertex.z);
-			maxZ = max(maxZ, vertex.z);
-		
-		var bounds = AABB(Vector3(minX, 0, minZ), Vector3(maxX, 0, maxZ));
-
-		# Tant que le point aléatoire n'est pas atteignable, continuer à en générer un nouveau
-		while true:
-			# Générer un point aléatoire à l'intérieur des limites rectangulaires
-			var random_point = Vector3(
-				randi_range(bounds.position.x, bounds.position.x + bounds.size.x),
-				0,
-				randi_range(bounds.position.z, bounds.position.z + bounds.size.z)
-			);
-			var map_point = gridmap_controller.global_to_local(random_point);
-			var cell_item = gridmap_controller.get_cell_item(map_point);
-			if cell_item == 0:
-				print(map_point);
-				print(random_point)
-				return random_point;
+func get_random_reachable_point(start_position: Vector3, gridmap_controller: GridMapController):
+	var cells = gridmap_controller.get_used_cells();
+	var around_gap = 5;
+	var around_cells = cells.filter(func (cell: Vector3):
+		var is_around_x = abs(start_position.x - cell.x);
+		var is_around_y = abs(start_position.y - cell.y);
+		return is_around_x < around_gap and is_around_y < around_gap;
+	);
+	
+	var random_point = around_cells[randi_range(0, around_cells.size()-1)];
+	var cell_item = gridmap_controller.get_cell_item(random_point);
+	if cell_item == 0:
+		return random_point;
--- PNJs/Actions/WaitAction.gd
+++ PNJs/Actions/WaitAction.gd
@@ -8,13 +8,14 @@ func execute(params: Dictionary) -> void:
 	
 	_update_target_position(params.start_position, params.gridmap_controller);
 	await navigation_agent.target_reached;
-	
+	var timer: SceneTreeTimer = params.gridmap_controller.get_tree().create_timer(1);
+	await timer.timeout;
 	var next_action: Action = null;
 	if target == 'Food':
 		next_action = Actions.get_action_by_id(Actions.BUY);
 	on_action_finished.emit(id, next_action);
 
 func _update_target_position(start_position: Vector3, gridmap_controller: GridMapController):
-	var target_position: Vector3 = NodeUtils.get_random_reachable_point(navigation_mesh, navigation_agent, gridmap_controller);
+	var target_position: Vector3 = NodeUtils.get_random_reachable_point(start_position, gridmap_controller);
 	navigation_agent.target_position = target_position;
 
