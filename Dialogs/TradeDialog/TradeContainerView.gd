2f7ca5e08def91d7f155c1f9e8a125e61dafc173
Etienne SC <etienne.salupocorvo@gmail.com>
2023-06-29 09:15:47 +0200

    feat : wip popup trade player

--- Dialogs/TradeDialog/TradeContainerView.gd
+++ Dialogs/TradeDialog/TradeContainerView.gd
@@ -1,27 +1,27 @@
 extends InventoryView
 class_name TradeContainerView
 
-@onready var item_texture_rect: TextureRect = $TradeItemContainer/TextureRect;
-@onready var desired_price_value: Label = $TradeItemContainer/DesiredPriceVBoxContainer/TextureRect/Value;
-@onready var amount_value = $TradeItemContainer/AmountVBoxContainer2/TextureRect/Value;
-@onready var trade_item_container = $TradeItemContainer;
-@onready var total_label = $TotalLabel;
-@onready var remove_button = $TradeItemContainer/RemoveButton;
+@export var item_texture_rect: TextureRect;
+@export var desired_price_value: Label;
+@export var amount_value: Label;
+@export var trade_item_container: Control;
+@export var total_label: Label;
+@export var remove_button: TextureButton;
 
 var desired_price_items := {};
 var _current_item_id: String;
-var _merchant_container_id: String;
-var _player_container_id: String;
+var _seller_container_id: String;
+var _buyer_container_id: String;
 var total_price = 0;
 signal update_containers_views;
 
 func _ready():
 	trade_item_container.visible = false;
 
-func _init_container(trade_container_id: String, merchant_container_id: String, player_container_id: String):
+func _init_container(trade_container_id: String, seller_container_id: String, buyer_container_id: String):
 	container_id = trade_container_id;
-	_merchant_container_id = merchant_container_id;
-	_player_container_id = player_container_id;
+	_seller_container_id = seller_container_id;
+	_buyer_container_id = buyer_container_id;
 	_load_container_config();
 	var items = MarketController.get_items(trade_container_id);
 	_update_items(items);
@@ -74,15 +74,15 @@ func _compute_total():
 func _on_validate_trade_pressed():
 	var slots = ContainersController.get_container_data(container_id);
 	_compute_total();
-	if MarketController.have_enough(_player_container_id, MarketController.MONEY_ITEM_ID, total_price):
+	if MarketController.have_enough(_buyer_container_id, MarketController.MONEY_ITEM_ID, total_price):
 		for x in slots:
 			for y in slots[x]:
 				var item = slots[x][y];
 				if not item.is_empty():
-					ContainersController.add_item([_player_container_id], item.id, item.amount);
+					ContainersController.add_item([_buyer_container_id], item.id, item.amount);
 					ContainersController.remove_item([container_id], item.id, item.amount);
-		ContainersController.remove_item([_player_container_id], MarketController.MONEY_ITEM_ID, total_price);
-		ContainersController.add_item([_merchant_container_id], MarketController.MONEY_ITEM_ID, total_price);
+		ContainersController.remove_item([_buyer_container_id], MarketController.MONEY_ITEM_ID, total_price);
+		ContainersController.add_item([_seller_container_id], MarketController.MONEY_ITEM_ID, total_price);
 	else:
 		NotificationEvents.notify.emit(NotificationEvents.NotificationType.ERROR, 'MARKET.NOT_ENOUGH');
 	_current_item_id = '';
@@ -91,14 +91,14 @@ func _on_validate_trade_pressed():
 func _on_remove_button_pressed():
 	var item_data = ContainersController.find_item_in_containers([container_id], _current_item_id).duplicate(true);
 	ContainersController.remove_item([container_id], item_data.item.id, item_data.item.amount);
-	ContainersController.add_item([_merchant_container_id], item_data.item.id, item_data.item.amount);
+	ContainersController.add_item([_seller_container_id], item_data.item.id, item_data.item.amount);
 	_current_item_id = '';
 	update_containers_views.emit();
 
 func _on_amount_minus_button_pressed():
 	var item_data = ContainersController.find_item_in_containers([container_id], _current_item_id).duplicate(true);
 	ContainersController.remove_item([container_id], item_data.item.id, 1);
-	ContainersController.add_item([_merchant_container_id], item_data.item.id, 1);
+	ContainersController.add_item([_seller_container_id], item_data.item.id, 1);
 	if item_data.item.amount == 1:
 		_current_item_id = '';
 	update_containers_views.emit();
@@ -106,8 +106,8 @@ func _on_amount_minus_button_pressed():
 func _on_amount_plus_button_pressed():
 	var item_data = ContainersController.find_item_in_containers([container_id], _current_item_id).duplicate(true);
 	if not item_data.is_empty():
-		if MarketController.have_enough(_merchant_container_id, item_data.item.id, 1):
-			ContainersController.remove_item([_merchant_container_id], item_data.item.id, 1);
+		if MarketController.have_enough(_seller_container_id, item_data.item.id, 1):
+			ContainersController.remove_item([_seller_container_id], item_data.item.id, 1);
 			ContainersController.add_item([container_id], item_data.item.id, 1);
 	update_containers_views.emit();
 
--- Dialogs/TradeDialog/TradeView.gd
+++ Dialogs/TradeDialog/TradeView.gd
@@ -1,17 +1,20 @@
 extends Panel
 
+@export var _trade_player_container_id: String = "player_dynamic_trade";
 @export var _trade_container_id: String = "dynamic_trade";
+@export var info_panel: Panel;
 
 @onready var merchant_container_view: MerchantContainerView = $HBox/MerchantContainerView;
 @onready var trade_container_view: TradeContainerView = $HBox/TradeContainerView;
+@onready var trade_player_container_view: TradeContainerView = $HBox/TradePlayerContainerView;
 @onready var player_container_view: PlayerContainerView = $HBox/PlayerContainerView;
-@onready var info_panel = %InfoPanel;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false;
 	player_container_view.update_containers_views.connect(_on_update_views);
 	trade_container_view.update_containers_views.connect(_on_update_views);
+	trade_player_container_view.update_containers_views.connect(_on_update_views);
 	merchant_container_view.update_containers_views.connect(_on_update_views);
 
 func _on_update_views() -> void:
@@ -21,12 +24,20 @@ func _on_update_views() -> void:
 	player_container_view._update_items(player_slots);
 	var trade_slots = ContainersController.get_container_data(trade_container_view.container_id);
 	trade_container_view._update_items(trade_slots);
+	var trade_player_slots = ContainersController.get_container_data(trade_player_container_view.container_id);
+	trade_player_container_view._update_items(trade_player_slots);
 
 func open(merchant_container_id: String) -> void:
 	ContainersController.register_container(_trade_container_id,\
 		2, trade_container_view._items_container.columns, {}, "dynamic");
+	ContainersController.register_container(_trade_player_container_id,\
+		2, trade_player_container_view._items_container.columns, {}, "player_dynamic");
 	player_container_view._init_container();
 	merchant_container_view._init_container(_trade_container_id, merchant_container_id, player_container_view.container_id);
 	trade_container_view._init_container(_trade_container_id, merchant_container_id, player_container_view.container_id);
+	trade_player_container_view._init_container(_trade_player_container_id, player_container_view.container_id, merchant_container_id);
 	info_panel.global_position = global_position - Vector2(info_panel.size.x + 30, 0);
 	visible = true;
+
+func _on_close_button_pressed():
+	visible = false;
