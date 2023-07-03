9133a65421b03923376d1e199799d0ca976d3854
Majstruk Guillaume <guillaume.majstruk@hotmail.fr>
2023-07-03 16:57:00 +0200

    feat: add DialogNode as own script

---
+++ Dialogs/DialogNode.gd
@@ -0,0 +1,24 @@
+class_name DialogNode
+
+var _id: String;
+var _type: String;
+var _next_nodes: Array;
+var value: String;
+var callback_name: String;
+var callback_params: Array;
+
+var next_nodes: Array:
+	get:
+		return _next_nodes;
+
+var id: String:
+	get:
+		return _id;
+
+func _init(id: String, val: String, type: String, cb_name: String = '', cb_params: Array = [], next_nodes: Array = []):
+	_id = id;
+	value = val;
+	_type = type;
+	_next_nodes = next_nodes;
+	callback_name = cb_name;
+	callback_params = cb_params;
