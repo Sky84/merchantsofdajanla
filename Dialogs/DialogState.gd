d7e1cd66ef477f093657f0c85f708ecf0bd285b4
Majstruk Guillaume <guillaume.majstruk@hotmail.fr>
2023-07-03 16:57:12 +0200

    feat: add DialogState as own script

---
+++ Dialogs/DialogState.gd
@@ -0,0 +1,29 @@
+class_name DialogState
+
+var _questions: Dictionary = {};
+var _answers: Dictionary = {};
+var _current: DialogNode = null;
+
+var current: DialogNode:
+	get:
+		return _current;
+
+func _init(questions: Dictionary, answers: Dictionary):
+	_questions = questions;
+	_answers = answers;
+	_load_current("1");
+
+func _load_current(id: String) -> void:
+	if not id.length():
+		_current = null;
+		return;
+	var current_question = _questions[id];
+	var current_answers = [];
+	for answer_id in current_question.next_nodes:
+		var current_answer = _answers[answer_id];
+		current_answers.push_back(DialogNode.new(answer_id, current_answer.value, "answer", current_answer.callback, current_answer.params, current_answer.next_nodes));
+	_current = DialogNode.new(id, current_question.value, "question", "", [], current_answers);
+
+func get_next_node(selected_answer_id: String) -> DialogNode:
+	_load_current(selected_answer_id);
+	return _current;
