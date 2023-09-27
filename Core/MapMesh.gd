1bcfb1c5771863f112e386bea534dd1edf84f183
Etienne Salupo-Corvo <etienne.salupocorvo@gmail.com>
2023-09-27 23:09:11 +0200

    feat: handling atlas size with map mesh

--- Core/MapMesh.gd
+++ Core/MapMesh.gd
@@ -1,6 +1,7 @@
 @tool
 extends MeshInstance3D
 
+@export var tile_atlas_size: float = 32;
 @export var init_atlas: bool:
 	get:
 		return false;
@@ -35,5 +36,10 @@ func _update_for_atlas():
 		new_material.uv1_offset.x = _texture.region.position.x / _texture.atlas.get_width();
 		new_material.uv1_offset.y = _texture.region.position.y / _texture.atlas.get_height();
 		new_material.uv1_scale.x = 0.2;
-		new_material.uv1_scale.y = 0.05;
+		var tile_number_width = _texture.region.size.x / tile_atlas_size;
+		var tile_number_height = _texture.region.size.y / tile_atlas_size;
+		new_material.uv1_scale.x = (tile_atlas_size * tile_number_width) /  _texture.atlas.get_width();
+		new_material.uv1_scale.y = (tile_atlas_size * tile_number_height) /  _texture.atlas.get_height();
 		set_surface_override_material(0, new_material);
+		mesh.size.x = tile_number_width;
+		mesh.size.y = tile_number_height;
