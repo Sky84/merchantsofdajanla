cf198ad91254a6069c380b6327de61686f399fba
Etienne Salupo-Corvo <etienne.salupocorvo@gmail.com>
2024-06-08 01:57:33 +0200

    feat: connect doors to other pathfinding points

--- Core/PathFinding.gd
+++ Core/PathFinding.gd
@@ -12,6 +12,7 @@ var _debug: bool = false;
 
 var pathfinding: AStar3D = AStar3D.new();
 var points := {};
+var reachable_object_points = [];
 
 var debug_cubes = {};
 
@@ -28,6 +29,7 @@ func update_pathfinding(chunks, chunk_tile_size: int, _tile_size: int):
 			for reachable_object in get_tree().get_nodes_in_group('reachable_objects'):
 				if not (reachable_object.global_position in points):
 					_add_point(reachable_object.global_position);
+					reachable_object_points.append(reachable_object.global_position);
 			for tile_x in range(0, chunk_tile_size, gap_between_points):
 				for tile_z in range(0, chunk_tile_size, gap_between_points):
 					var point_position = Vector3(
@@ -38,7 +40,7 @@ func update_pathfinding(chunks, chunk_tile_size: int, _tile_size: int):
 					if point_position in points or _is_object_on_point(chunk, point_position):
 						continue;
 					_add_point(point_position);
-	_connect_points();
+	_connect_all_points();
 
 func _add_point(point_position: Vector3):
 	var id = pathfinding.get_available_point_id();
@@ -51,7 +53,7 @@ func _add_point(point_position: Vector3):
 		debug_mesh.global_position = point_position;
 
 func _is_object_on_point(chunk: ChunkController, point_position: Vector3):
-	var result = false;
+	var result := false;
 	for key in chunk.chunk_objects:
 		var object = chunk.chunk_objects[key];
 		var object_position = Vector3(object.global_position.x, 0, object.global_position.z);
@@ -71,7 +73,7 @@ func _is_object_on_point(chunk: ChunkController, point_position: Vector3):
 			break;
 	return result;
 
-func _connect_points():
+func _connect_all_points():
 	var neighbors_to_connect = [-gap_between_points, 0, gap_between_points];
 	for point in points:
 		for x in neighbors_to_connect:
@@ -82,10 +84,16 @@ func _connect_points():
 					continue;
 				var neighbor_point = point + Vector3(x, 0, z);
 				if neighbor_point in points:
-					var current_id: int = points[point];
-					var neighbor_id: int = points[neighbor_point];
-					if not pathfinding.are_points_connected(current_id, neighbor_id):
-						pathfinding.connect_points(current_id, neighbor_id);
+					_connect_points(point, neighbor_point);
+		for reachable_object_point in reachable_object_points:
+			if point.distance_to(reachable_object_point) < gap_between_points*2:
+				_connect_points(point, reachable_object_point);
+
+func _connect_points(point: Vector3, neighbor_point: Vector3):
+	var current_id: int = points[point];
+	var neighbor_id: int = points[neighbor_point];
+	if not pathfinding.are_points_connected(current_id, neighbor_id) and current_id != neighbor_id:
+		pathfinding.connect_points(current_id, neighbor_id);
 
 func find_path(from: Vector3, to: Vector3) -> Array:
 	var start: int = pathfinding.get_closest_point(from);
