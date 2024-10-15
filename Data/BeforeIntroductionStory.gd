852cf2c761f1f4946ff7851763f2f914562703e9
Etienne Salupo-Corvo <etienne.salupocorvo@gmail.com>
2024-10-15 15:36:26 +0200

    spawn in house at intro scene

--- Data/BeforeIntroductionStory.gd
+++ Data/BeforeIntroductionStory.gd
@@ -6,11 +6,9 @@ func play_state(game_map_controller: GameMapController, player: Player) -> void:
 	player.global_position = _cut_scene_player_position;
 	print('BeforeIntro')
 	await player.get_tree().create_timer(1.0).timeout;
-	print('BeforeIntro ater')
 	var city: ChunkController = game_map_controller.get_city('CITY_1');
-	print(city.get_node("Chunk"));
-	var _house_exterior: ExteriorHouseController = city.get_node('Chunk/MapDecorations/House2');
+	var _house_exterior: ExteriorHouseController = city.get_node('MapDecorations/House2');
 	player._nearest_interactive = _house_exterior.door_instance;
-	player._nearest_interactive.interact(player);
+	player._nearest_interactive.interact(player._owner_id);
 	player._nearest_interactive = null;
 	
